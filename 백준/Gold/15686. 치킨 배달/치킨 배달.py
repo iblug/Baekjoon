284a4c62189fdfb3a4e62e67e1b0798fccda24a2
NimbuS <114655005+iblug@users.noreply.github.com>
2022-12-29 20:25:00 +0900

    [Gold V] Title: 치킨 배달, Time: 452 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+
+n, m = map(int, input().split())
+data = []
+jip , chi = 0, 0 # 갯수
+jj = [] # 집의 좌표 정보
+cc = [] # 치킨집의 좌표 정보
+for i in range(n):
+    data.append(list(map(int, input().split())))
+    for j in range(n):
+        if data[i][j] == 1:
+            jip += 1
+            jj.append([i, j])
+        if data[i][j] == 2:
+            chi += 1
+            cc.append([i, j])
+
+graph = [[0] * jip for _ in range(chi)] # 거리정보
+geo = []
+for i in range(chi):
+    for j in range(jip):
+        graph[i][j] = abs(cc[i][0] - jj[j][0]) + abs(cc[i][1] - jj[j][1])
+
+candidate = list(combinations(range(chi),m))
+
+count = 0
+mm = 99
+r = 9999
+for x in candidate:
+    for i in range(jip):
+        for y in range(m):
+            mm = min(graph[x[y]][i], mm)
+        count += mm
+        mm = 99
+    r = min(count, r)
+    count = 0
+print(r)
