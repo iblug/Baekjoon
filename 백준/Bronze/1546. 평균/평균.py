2f9e44f2fdeb0088065ad3dc8bac0229fc58296c
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-02 20:05:29 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,7 +1,4 @@
 n = int(input())
 score = list(map(int, input().split()))
-r = 0
 
-for i in score:
-    r += i / max(score) * 100
-print(r/len(score))
+print(sum(score) / max(score) * 100/len(score))
