d42625f6f237be793ed725ce04584a6f7a3d0171
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-02 20:41:23 +0900

    [Silver V] Title: 셀프 넘버, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,12 +1,15 @@
-a = [i for i in range(1, 10000)]
-for i in range(1, 10000):
-    while(1):
-        c = i
-        for _ in range(4):
-            c += i%10
-            i //= 10
-        if c in a:
-            a.remove(c)
-        else:
-            break
-print(*a,sep='\n')
+data = [0 for _ in range(10001)]
+def d(x):
+    r = x
+    while x > 0:
+        r += x%10
+        x //= 10
+    return int(r)
+
+for i in range(1, 10001):
+    c = d(i)
+    if c <= 10000:
+        data[c] = 1
+for i in range(1, 10001):
+    if data[i] != 1:
+        print(i)
