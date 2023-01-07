5de7dcd10f5349507056a8939b1e952ccb7be67d
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-07 21:34:24 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -1,12 +1,11 @@
 t = int(input())
 for i in range(t):
     n = int(input())
-    top = ''
     top_l = 0
     for j in range(n):
         s , l = input().split()
         l = int(l)
-        top_l = max(l, top_l)
-        if l == top_l:
+        if l > top_l:
+            top_l = l
             top = s
     print(top)
