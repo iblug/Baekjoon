844dff42cfcef0c1bcc45d2ca5b0f9bb4eb2e6b0
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-08 15:12:02 +0900

    [Bronze III] Title: Baseball, Time: 76 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for _ in range(t):
+    yt, kt = 0, 0
+    for _ in range(9):
+        y , k = map(int, input().split())
+        yt += y
+        kt += k
+    else:
+        if yt > kt:
+            print('Yonsei')
+        elif kt > yt:
+            print('Korea')
+        else:
+            print('Draw')
+        
