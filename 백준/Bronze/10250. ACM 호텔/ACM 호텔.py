781cc5b63a37f61b59408a3712a60b76cdb986f5
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-11 23:53:07 +0900

    [Bronze III] Title: ACM 호텔, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for _ in range(T):
+    h, w, n = map(int, input().split())
+    if n % h == 0:
+        c = h
+        r = n // h
+    else:
+        c = n % h
+        r = n // h + 1
+    print(100*c + r)
