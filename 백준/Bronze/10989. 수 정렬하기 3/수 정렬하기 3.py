5d74e47ecbaa4770dec1621134268a1e8af48185
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-16 22:00:03 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9384 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(input())
+
+result = [0] * 10001
+
+for _ in range(N):
+    x = sys.stdin.readline()
+    result[int(x)] += 1
+for i in range(1, 10001):
+    for _ in range(result[i]):
+        print(i)
