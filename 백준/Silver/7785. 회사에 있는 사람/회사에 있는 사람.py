a8af83357e40580cfddb93b05fe66cd01e7080eb
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-19 15:19:32 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 156 ms, Memory: 48656 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+is_in = {}
+for _ in range(N):
+    a, b = input().split()
+    is_in[a] = is_in.get(a, b)
+    is_in[a] = b
+result = [i for i in is_in if is_in[i] == 'enter']
+
+print('\n'.join(sorted(result, reverse=True)))
