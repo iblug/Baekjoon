34b47e4a55c8a65911b298010ae72734aaa06bb6
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-21 01:33:45 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 128 ms, Memory: 49236 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,8 +1,5 @@
-import sys
-def main() ->None:
- _,*n=open(0)
- r={}
- for j in n:r[j[:-7]]=j[-2];
- print('\n'.join(sorted([i for i in r if r[i]=='r'],reverse=1)))
-if __name__=="__main__":
-    sys.exit(main())
+_,*n=open(0)
+r={}
+for j in n:
+ r[j[:-7]]=j[-2]
+print('\n'.join(sorted([i for i in r if r[i]=='r'],reverse=1)))
