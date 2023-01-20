89c8d1f4f389f76a90d5664daf515f62e73fbcd2
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-21 04:13:40 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 68 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+N, K = map(int, input().split())
+data = [i for i in range(1, N+1)]
+index = K-1
+r = deque()
+n_ = N
+k_ = K
+data_ = data
+while len(r) < n_:
+    r.append(data.pop(index))
+    N-=1
+    if N > 0:
+        index = (index+K)%N - 1
+    else:
+        index = 0
+    if index <0:
+        index+=N
+print('<'+', '.join(map(str, r))+'>')
