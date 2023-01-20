506a090080216f3324c5fb8cf4ba85d363743176
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-20 13:08:45 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 136 ms, Memory: 42512 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -6,6 +6,6 @@ N = int(input())
 r = {}
 for _ in range(N):
     a = input().rstrip()
-    r[a[:-6]] = a[-1:]
+    r[a[:-6]] = a[-1]
 c = [i for i in r if r[i] == 'r']
 print('\n'.join(sorted(c, reverse=True)))
