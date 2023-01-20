cfe573ffa8ed4d5783744828645b09a38d0f365c
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-20 13:29:53 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 136 ms, Memory: 42316 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,10 +1,8 @@
 import sys
-
-input = sys.stdin.readline
-
-N = int(input())
-r = {}
+input=sys.stdin.readline
+N=int(input())
+r={}
 for _ in range(N):
-    a = input().rstrip()
-    r[a[:-6]] = a[-1]
-print('\n'.join(sorted([i for i in r if r[i] == 'r'], reverse=True)))
+ a=input().rstrip()
+ r[a[:-6]]=a[-1]
+print('\n'.join(sorted([i for i in r if r[i]=='r'],reverse=1)))
