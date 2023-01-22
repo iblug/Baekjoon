0227be586a7c93ac565b003355a7f291fbc23c69
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-22 17:39:47 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,9 +1,8 @@
-from itertools import combinations
+data = set(int(input()) for _ in range(9))
 
-data = [int(input()) for _ in range(9)]
-coms = combinations(data, 7)
-
-for com in coms:
-    if sum(com) == 100:
-        print(*sorted(com), sep='\n')
+over = sum(data) - 100
+for i in data:
+    if (over - i) in data:
+        result = sorted(data-{i, over-i})
         break
+print(*result, sep='\n')
