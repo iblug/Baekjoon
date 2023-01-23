abd6e62a9a7578b76b969ba169b4c6774a55ddac
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-23 13:13:11 +0900

    [Bronze II] Title: 시험 감독, Time: 532 ms, Memory: 149916 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -3,13 +3,10 @@ input = sys.stdin.readline
 N = int(input())
 A = list(map(int, input().split()))
 B, C = map(int, input().split())
-cnt = 0
+r = []
 for a in A:
-    a -=B
-    cnt += 1
-    if a > 0:
-        if a % C == 0:
-            cnt += a//C
-        else:
-            cnt += a // C + 1
-print(cnt)
+    if a > B:
+        r.append(1 - (B-a)//C)
+    else:
+        r.append(1)
+print(sum(r))
