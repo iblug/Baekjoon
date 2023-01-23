2b1bed995c9121979f15633fa8bf78f4276649c2
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-23 14:58:10 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1008 ms, Memory: 149892 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,6 +1,3 @@
-import sys
-input = sys.stdin.readline
-n = int(input())
-a = [int(input()) for _ in range(n)]
+n,*a=map(int,open(0).read().split())
 a.sort()
-print('\n'.join(map(str, a)))
+print('\n'.join(map(str,a)))
