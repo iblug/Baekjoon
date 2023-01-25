b679499f2aec6a3d81038311213b3b4aacd6f423
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-25 15:06:28 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 80 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,32 +1,62 @@
 import sys
 input = sys.stdin.readline
+def check1(i):
+    while i < l:
+        if a[i] == '(':
+            i += 1
+            i = check1(i)
+        elif a[i] == ')':
+            return i+1
+        elif a[i] == '[':
+            i += 1
+            i = check2(i)
+        elif a[i] == ']':
+            break
+        else:
+            i += 1
+        if i == False:
+            return False
+    return False
+
+def check2(i):
+    while i < l:
+        if a[i] == '(':
+            i += 1
+            i = check1(i)
+        elif a[i] == ']':
+            return i+1
+        elif a[i] == '[':
+            i += 1
+            i = check2(i)
+        elif a[i] == ')':
+            break
+        else:
+            i += 1
+        if i == False:
+            return False
+    return False
+
 while True:
     a = input().rstrip()
     if a == '.':
         break
-    c = 'yes'
-    stack = []
-    for i in a:
-        if i == '(' or  i == '[':
-            stack.append(i)
-        elif i == ')':
-            if stack:
-                t = stack.pop()
-                if t != '(':
-                    c = 'no'
-                    break
-            else:
-                c = 'no'
-                break
-        elif i == ']':
-            if stack:
-                t = stack.pop()
-                if t != '[':
-                    c = 'no'
-                    break
-            else:
-                c = 'no'
-                break
-    if stack:
-        c = 'no'
-    print(c)
+    c = True
+    l = len(a)
+    i = 0
+    while i < l:
+        if a[i] == '(':
+            i += 1
+            i = check1(i)
+        elif a[i] == '[':
+            i += 1
+            i = check2(i)
+        elif a[i] == ')' or a[i] == ']':
+            i = False
+        else:
+            i += 1
+        if i == False:
+            break
+    if i == False:
+        print('no')
+    else:
+        print('yes')
