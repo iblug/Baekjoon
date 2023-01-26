a64519b670b0d6fb365f785d8309054a5a247448
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-26 13:56:58 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 80 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,13 @@
+coins = [0] * 4
+Q = 0.25
+D = 0.1
+N = 0.05
+P = 0.01
+coin = [Q, D, N, P]
+t = int(input())
+for _ in range(t):
+    c = int(input())
+    for i in range(4):
+        coins[i] = int(c // (coin[i] * 100))
+        c %= (coin[i] * 100)
+    print(*coins)
