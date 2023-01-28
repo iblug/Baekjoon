cafbb073a9c57edb897793251631e822faede3a1
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-28 20:44:14 +0900

    [Silver II] Title: 랜선 자르기, Time: 60 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+def bin(arr, start, end):
+    mid = (end + start) // 2
+    if mid == 0:
+        mid = 1
+    cnt = 0
+    for i in arr:
+        cnt += i // mid
+    
+    if cnt < n:
+        end = mid - 1
+        bin(arr, start, end)
+    elif cnt >= n:
+        result.append(mid)
+        start = mid + 1
+        if start > end:
+            return True
+        bin(arr, start, end)
+    return True
+
+k , n = map(int, input().split())
+data = [int(input()) for _ in range(k)]
+longest = max(data)
+result = []
+bin(data, 0, longest)
+print(max(result))
