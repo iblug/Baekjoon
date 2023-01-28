505cf8cf613c0fd0598d4ab2ae035a223ab13aa9
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-28 22:05:58 +0900

    [Silver II] Title: 최대 힙, Time: 112 ms, Memory: 43912 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,14 +1,8 @@
-import sys, heapq
-input = sys.stdin.readline
-
-n = int(input())
-h = []
-for _ in range(n):
-    i = int(input())
-    if i == 0:
-        if h:
-            print(-heapq.heappop(h))
-        else:
-            print(0)
-    else:
-        heapq.heappush(h, -i)
+import heapq
+_,*d=open(0)
+h=[]
+for i in d:
+ if i[0] == '0':
+  if h:print(-heapq.heappop(h))
+  else:print(0)
+ else:heapq.heappush(h,-int(i))
