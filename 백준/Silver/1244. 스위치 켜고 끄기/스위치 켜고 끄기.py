1fdf318c295085aa54d3e5d39ff2e1a84a18ed7f
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-31 01:33:59 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -2,13 +2,13 @@ import sys
 input = sys.stdin.readline
 
 def man(c):
-    for cm in range(c, n, c+1):
+    for cm in range(c, n+1, c):
         sw[cm] = (sw[cm] + 1)%2
 
 def woman(c):
     sw[c] = (sw[c]+1)%2
     for cw in range(1, n//2+1):
-        if c-cw < 0 or c+cw >= n:
+        if c-cw <= 0 or c+cw > n:
             break
         if sw[c-cw] == sw[c+cw]:
             sw[c-cw] = (sw[c-cw]+1)%2
@@ -17,19 +17,14 @@ def woman(c):
             break
 
 n = int(input())
-sw = list(map(int, input().split()))
+sw = [0] + list(map(int, input().split()))
 
 st = int(input())
 for _ in range(st):
     g, c = map(int, input().split())
     if g == 1:
-        man(c-1)
+        man(c)
     elif g == 2:
-        woman(c-1)
-ic = 1
-for i in range(n):
-    print(sw[i], end=' ')
-    ic += 1
-    if ic > 20:
-        print()
-        ic = 1
+        woman(c)
+for i in range(1, n, 20):
+    print(*sw[i:i+20])
