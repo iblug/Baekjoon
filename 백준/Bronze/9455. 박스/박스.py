499983bec1c82d943fbde1e29947452b3e387b0f
NimbuS <114655005+iblug@users.noreply.github.com>
2023-01-31 16:14:12 +0900

    [Bronze I] Title: 박스, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9455. 박스/박스.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    m, n = map(int, input().split())
+    data = [list(input().rstrip().split()) for _ in range(m)]
+    total = 0
+    for n1 in range(n):
+        cnt0 = cnt1 = 0
+        sum_ = 0
+        for m1 in range(m):
+            if data[m1][n1] == '1':
+                cnt1 += 1
+            elif data[m1][n1] == '0':
+                if cnt1 > 0:
+                    sum_ += cnt1
+        total += sum_
+    print(total)
