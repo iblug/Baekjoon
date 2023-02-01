5f7d428abc7e2b89d16353f58b248e3e4ff7f32e
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-01 13:54:19 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 164 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -1,11 +1,12 @@
 n = int(input())
+data = ['0', '1', '2', '3', '5', '6', '8', '9']
 
 while True:
     n_ = str(n)
-    n_ = n_.replace('7', '')
-    n_ = n_.replace('4', '')
-    if n_:
-        n -= 1
+    for d in data:
+        if d in n_:
+            n -= 1
+            break
     else:
         print(n)
         break
