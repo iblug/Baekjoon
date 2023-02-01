893098dc6e4b6316ee7d04677f6fec45fc7475c6
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-01 11:02:26 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 37628 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,11 +1,11 @@
 n, m = map(int, input().split())
 c = list(map(int, input().split()))
 
-max_value = 9
+max_value = []
 for i in range(n-2):
     for j in range(i+1, n-1):
         for k in range(j+1, n):
             now = c[i] + c[j] + c[k]
             if now <= m:
-                max_value = max(now, max_value)
-print(max_value)
+                max_value.append(now)
+print(max(max_value))
