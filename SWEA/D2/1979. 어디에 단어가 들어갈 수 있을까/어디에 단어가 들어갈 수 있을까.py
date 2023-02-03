4ef53bc98c7cec058f863bde64141bcff7ebbb83
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-03 23:03:23 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 161 ms, Memory: 60,960 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,51 +1,36 @@
 T = int(input())
+
 for t in range(T):
     n, k = map(int, input().split())
     d = [list(map(int, input().split())) for _ in range(n)]
+    key = [1]*k
+
     cnt = 0
     for i in d:
-        j = 0
-        while j < n: 
-            if i[j] == 1:
-                if j < n-k+1:
-                    for a in range(j, j+k):
-                        if i[a] != 1:
-                            break
-                    else:
-                        if j == n-k:
-                            cnt += 1
-                        elif i[a+1] == 0:
-                            cnt += 1
-                            j += k
-                        else:
-                            j += k
-                            while j < n:
-                                if i[j] == 1:
-                                    j += 1
-                                else:
-                                    break
-            j += 1
-    d_ = list(zip(*d))
-    for i in d_:
-        j = 0
-        while j < n: 
-            if i[j] == 1:
-                if j < n-k+1:
-                    for a in range(j, j+k):
-                        if i[a] != 1:
-                            break
-                    else:
-                        if j == n-k:
-                            cnt += 1
-                        elif i[a+1] == 0:
-                            cnt += 1
-                            j += k
-                        else:
-                            j += k
-                            while j < n:
-                                if i[j] == 1:
-                                    j += 1
-                                else:
-                                    break
-            j += 1
+        for j in range(0,n-k):
+            if j == 0:
+                if i[:k+1] == key + [0]:
+                    cnt += 1
+            if j == n-k-1:
+                if i[n-k-1:] == [0] + key:
+                    cnt += 1
+            if i[j:j+k+2] == [0] + key + [0]:
+                cnt += 1
+        if n == k:
+            if i == key:
+                cnt += 1
+    d = list(map(list, zip(*d)))
+    for i in d:
+        for j in range(0,n-k):
+            if j == 0:
+                if i[:k+1] == key + [0]:
+                    cnt += 1
+            if j == n-k-1:
+                if i[n-k-1:] == [0] + key:
+                    cnt += 1
+            if i[j:j+k+2] == [0] + key + [0]:
+                cnt += 1
+        if n == k:
+            if i == key:
+                cnt += 1
     print(f'#{t+1} {cnt}')
