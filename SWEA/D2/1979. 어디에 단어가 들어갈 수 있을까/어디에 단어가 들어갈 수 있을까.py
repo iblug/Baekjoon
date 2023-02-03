9d040b99a91331b4f2e49236ffc281253728fdfa
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-03 23:07:51 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 157 ms, Memory: 58,764 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,36 +1,29 @@
 T = int(input())
 
+def find(cnt):
+    for i in d:
+        for j in range(0,n-k):
+            if j == 0:
+                if i[:k+1] == key + [0]:
+                    cnt += 1
+            if j == n-k-1:
+                if i[n-k-1:] == [0] + key:
+                    cnt += 1
+            if i[j:j+k+2] == [0] + key + [0]:
+                cnt += 1
+        if n == k:
+            if i == key:
+                cnt += 1
+    return cnt
+
 for t in range(T):
     n, k = map(int, input().split())
     d = [list(map(int, input().split())) for _ in range(n)]
     key = [1]*k
 
-    cnt = 0
-    for i in d:
-        for j in range(0,n-k):
-            if j == 0:
-                if i[:k+1] == key + [0]:
-                    cnt += 1
-            if j == n-k-1:
-                if i[n-k-1:] == [0] + key:
-                    cnt += 1
-            if i[j:j+k+2] == [0] + key + [0]:
-                cnt += 1
-        if n == k:
-            if i == key:
-                cnt += 1
+    cnt = find(0)
+
     d = list(map(list, zip(*d)))
-    for i in d:
-        for j in range(0,n-k):
-            if j == 0:
-                if i[:k+1] == key + [0]:
-                    cnt += 1
-            if j == n-k-1:
-                if i[n-k-1:] == [0] + key:
-                    cnt += 1
-            if i[j:j+k+2] == [0] + key + [0]:
-                cnt += 1
-        if n == k:
-            if i == key:
-                cnt += 1
+    cnt = find(cnt)
+    
     print(f'#{t+1} {cnt}')
