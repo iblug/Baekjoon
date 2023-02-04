c43fd96833e75f935061654d0a9b4337c911ca70
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-04 19:58:07 +0900

    [Bronze I] Title: 색종이, Time: 156 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,13 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 d = [[0]*1001 for _ in range(1001)]
 
 n = int(input())
 
-result = [0]*(n+1)
 for c in range(1, n+1):
     x, y, a, b = map(int, input().split())
     for i in range(x, x+a):
         for j in range(y, y+b):
-            result[d[i][j]] -= 1
             d[i][j] = c
-            result[c] += 1
-print(*result[1:], sep='\n')
+for m in range(n):
+    sum_ = 0
+    for i in d:
+        sum_ += i.count(m+1)
+    print(sum_)
