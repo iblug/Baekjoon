674e4bc3a270bb1e9ff0472c57d38241d9688c50
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-05 15:55:21 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -5,8 +5,8 @@ t = int(input())
 d = [0] * 41
 d[0] = (1, 0)
 d[1] = (0, 1)
+for i in range(2, 41):
+    d[i] = (d[i-1][0]+d[i-2][0], d[i-1][1]+d[i-2][1])
 for _ in range(t):
     n = int(input())
-    for i in range(2, n+1):
-        d[i] = (d[i-1][0]+d[i-2][0], d[i-1][1]+d[i-2][1])
     print(*d[n])
