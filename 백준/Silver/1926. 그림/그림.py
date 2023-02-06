5d5bff2fb1fb0c25a988a235c4ae5cae5264fe8c
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-06 20:20:12 +0900

    [Silver I] Title: 그림, Time: 324 ms, Memory: 45088 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,27 +1,29 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(1000000)
 
 def dfs(x, y):
     global cnt
+    stack = [(x, y)]
     d[x][y] += 1
     cnt += 1
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if d[nx][ny] == 1:
-                d[nx][ny] = 2
-                dfs(nx, ny)
+    while stack:
+        x, y = stack.pop()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if d[nx][ny] == 1:
+                    stack.append((nx, ny))
+                    d[nx][ny] = 2
+                    cnt += 1
 
 n, m = map(int,input().split())
-
 d = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-result = [0]
+result = 0
 total = 0
 for x in range(n):
     for y in range(m):
@@ -29,6 +31,5 @@ for x in range(n):
             cnt = 0
             total += 1
             dfs(x, y)
-            result.append(cnt)
-print(total)
-print(max(result))
+            result = max(result, cnt)
+print(total, result,sep='\n')
