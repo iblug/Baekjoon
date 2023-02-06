6ff9c890fba51825130f08d3abd994264495d49d
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-06 14:08:45 +0900

    [Bronze I] Title: 행복한지 슬픈지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
+++ 백준/Bronze/10769. 행복한지 슬픈지/행복한지 슬픈지.py
@@ -1,7 +1,19 @@
 s = input()
-cnt1 = s.count(':-)')
-cnt2 = s.count(':-(')
 
+cnt1 = cnt2 = 0
+i = 0
+while True:
+    if len(s) < 3:
+        break
+    if s[i] == ':':
+        if s[i:i+3] == ':-)':
+            cnt1 += 1
+        elif s[i:i+3] == ':-(':
+            cnt2 += 1
+    i += 1
+    if i > len(s)-3:
+        break
+        
 if cnt1 == 0 and cnt2 == 0:
     print('none')
 elif cnt1 == cnt2:
