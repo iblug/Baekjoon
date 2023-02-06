6e32ef72e4102a06f064f8042c7b4bf8e14efa2e
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-06 15:38:06 +0900

    [Silver II] Title: 색종이 만들기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -3,34 +3,24 @@ input = sys.stdin.readline
 
 n = int(input())
 s = n
-graph = [list(input().split()) for _ in range(n)]
-cnt0 = cnt1 = 0
+graph = [list(map(int,input().split())) for _ in range(n)]
+cnt = [0, 0]
 while s > 0:
     for i in range(0,n,s):
         for j in range(0,n,s):
-            if graph[i][j] == '1':
+            key = graph[i][j]
+            if key == 2:
+                continue
+            for x in range(0, s):
+                for y in range(0, s):
+                    if graph[i+x][j+y] != key:
+                        break
+                if graph[i+x][j+y] != key:
+                    break
+            else:
+                cnt[key] += 1
                 for x in range(0, s):
                     for y in range(0, s):
-                        if graph[i+x][j+y] != '1':
-                            break
-                    if graph[i+x][j+y] != '1':
-                        break
-                else:
-                    cnt1 += 1
-                    for x in range(0, s):
-                        for y in range(0, s):
-                            graph[i+x][j+y] = 2
-            if graph[i][j] == '0':
-                for x in range(0, s):
-                    for y in range(0, s):
-                        if graph[i+x][j+y] != '0':
-                            break
-                    if graph[i+x][j+y] != '0':
-                        break
-                else:
-                    cnt0 += 1
-                    for x in range(0, s):
-                        for y in range(0, s):
-                            graph[i+x][j+y] = 2
+                        graph[i+x][j+y] = 2
     s //= 2
-print(cnt0, cnt1)
+print(*cnt, sep='\n')
