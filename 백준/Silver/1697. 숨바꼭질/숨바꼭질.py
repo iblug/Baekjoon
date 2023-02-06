3f6a9cfbae631bf20d3cb9c022ab5213d70ca084
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-06 14:49:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 176 ms, Memory: 55480 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,22 +1,20 @@
 from collections import deque
 
+
 n, k = map(int,input().split())
 m = max(n, k)
 g = [[i*2,i+1,i-1] for i in range(m+2)]
 vi = [1e9] * (m+2)
-visited = [False] * (m+2)
 def bfs(x,k):
     q = deque([x])
     vi[x] = 0
-    visited[x] = True
     while q:
         x = q.popleft()
         if x == k:
             return vi[x]
         for i in g[x]:
             if 0 <= i <= m+1:
-                if not visited[i]:
-                    visited[i] = True
+                if vi[i] == 1e9:
                     vi[i] = min(vi[i],vi[x] + 1)
                     q.append(i)
 print(bfs(n,k))
