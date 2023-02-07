5bd7570ba59405c47fe3c165e3f3666f1bf9051c
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-07 11:30:09 +0900

    [Silver IV] Title: 빙고, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+a, b = [], []
+for k in range(10):
+    if k < 5:
+        a += list(input().split())
+    else:
+        b += list(input().split())
+
+cnt = 0       
+for i in range(25):
+    idx = a.index(b[i])
+    a[idx] = 0
+    
+    if all(a[ny] == 0 for ny in range(25) if ny % 5 == idx % 5):
+        cnt += 1
+    if all(a[nx] == 0 for nx in range(25) if nx // 5 == idx // 5):
+        cnt += 1
+    if idx % 6 == 0 and all(a[n1] == 0 for n1 in range(25) if n1 % 6 == 0):
+        cnt += 1
+    if idx % 4 == 0 and all(a[n1] == 0 for n1 in range(4, 24, 4)) and idx != 0 and idx != 24:
+        cnt += 1
+        
+    if cnt >= 3:
+        break
+
+print(i+1)
