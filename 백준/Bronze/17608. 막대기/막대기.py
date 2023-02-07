0ff6d5152cc2d90979df57c4d815ef0562298712
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-07 16:19:22 +0900

    [Bronze II] Title: 막대기, Time: 128 ms, Memory: 37180 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -6,9 +6,8 @@ n = int(input())
 h = [int(input())]
 for _ in range(n-1):
     a = int(input())
-    while h[0] <= a:
+    while h and h[0] <= a:
         heapq.heappop(h)
-        if not h:
-            break
     heapq.heappush(h, a)
-print(len(h))
+    
+print(len(h))    
