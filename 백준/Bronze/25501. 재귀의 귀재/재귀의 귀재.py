2507c390da6980233cc3e0d5641c7be28de80d8c
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-08 18:08:51 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 472 ms, Memory: 31316 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def recursion(s, l, r):
+    global cnt
+    cnt += 1
+    if l >= r:
+        return 1
+    elif s[l] != s[r]:
+        return 0
+    else:
+        return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+t = int(input())
+for _ in range(t):
+    cnt = 0
+    s = input().rstrip()
+    print(isPalindrome(s), cnt)
