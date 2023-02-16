78079fac93ee57549f1faa4dc567b4bf3c4b85a5
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-16 11:26:13 +0900

    [Silver II] Title: 아카라카, Time: 76 ms, Memory: 35164 KB -BaekjoonHub

--- 백준/Silver/23304. 아카라카/아카라카.py
+++ 백준/Silver/23304. 아카라카/아카라카.py
@@ -1,23 +1,13 @@
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
-
 def a(s):
     if len(s) == 1:
         return True
     
-    l = len(s)
-    L = l//2
-    
-    if l & 1:
-        R = l//2+1
-    else:
-        R = l//2
+    l = len(s)//2
 
-    if s[:L] != s[R:]:
+    if s[:l] != s[-l:]:
         return False
     
-    return a(s[:L]) & a(s[R:])
+    return a(s[:l])
 
 S=input().rstrip()
 if a(S):
