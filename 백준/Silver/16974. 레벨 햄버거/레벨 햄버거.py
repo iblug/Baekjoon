e17a0e7f0df496c518ac4a7d67bc993726ab039c
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-16 19:44:34 +0900

    [Silver I] Title: 레벨 햄버거, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16974. 레벨 햄버거/레벨 햄버거.py
+++ 백준/Silver/16974. 레벨 햄버거/레벨 햄버거.py
@@ -4,19 +4,21 @@ def bur(n, x):
             return 0
         else:
             return 1
-    bb = 2**(n+1)-3
-    pp = 2**n-1
-    p = 2**(n+1)-1
+    bb = 2**(n+1)-3 # 이전 단계 버거크기
+    pp = 2**n-1     # 이전 단계 패티 개수
+    b = 2**(n+2)-3
+    p = 2**(n+1)-1  # 현재 단계 패티 개수
     
-    if x == 0:
+    # 분할
+    if x == 1:  # 왼쪽 끝
         return 0
-    elif bb+2 > x:
+    elif bb+2 > x:  # 왼쪽
         return bur(n-1, x-1)
-    elif bb+2 == x:
+    elif bb+2 == x: # 중앙
         return pp + 1
-    elif bb+2 < x:
+    elif bb+2 < x < b:  # 오른쪽
         return pp + 1 + bur(n-1, x-bb-2)
-    else:
+    else: #  끝
         return p
 
 N, X = map(int, input().split())
