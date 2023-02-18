0976ecb4383ba21a0c66c24a911bd0141f2e1f4f
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-18 14:46:51 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,6 +1,5 @@
 -- 코드를 입력하세요
 SELECT TRUNCATE(price / 10000,0)*10000 AS `price_group`, count(*) AS `products`
 FROM product
-GROUP BY TRUNCATE(price / 10000,0)
+GROUP BY `price_group`
 ORDER BY `price_group`;
-
