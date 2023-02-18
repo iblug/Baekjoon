38f2c56fa25e0f82a03002a9ac3993d830fed0a9
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-18 21:12:50 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 6, Time: 596 ms, Memory: 74372 KB -BaekjoonHub

---
+++ 백준/Silver/24484. 알고리즘 수업 － 깊이 우선 탐색 6/알고리즘 수업 － 깊이 우선 탐색 6.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def dfs(x,t):
+    global cnt
+    cnt += 1
+    d[x] = cnt*t
+    for c in graph[x]:
+        if not v[c]:
+            v[c] = True
+            dfs(c, t+1)
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+v = [False] * (n+1)
+d = [0] * (n+1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+for e in graph:
+    e.sort(reverse=True)
+
+cnt = 0
+v[r] = True
+dfs(r, 0)
+print(sum(d))
