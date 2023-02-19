69099e0888cc06d716fa71e2410cb7cf4f6cca12
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-20 01:21:26 +0900

    [Silver I] Title: 안전 영역, Time: 724 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -8,12 +8,12 @@ def dfs(x, y):
         if visited[x][y]:
             continue
         visited[x][y] = True
-        for idx in range(4):
-            nx = x + dx[idx]
-            ny = y + dy[idx]
+        for dx, dy in d:
+            nx = x + dx
+            ny = y + dy
             if 0 > nx or nx >= n or 0 > ny or ny >= n:
                 continue
-            if not visited[nx][ny] and graph[nx][ny] > d:
+            if not visited[nx][ny] and graph[nx][ny] > h:
                 st.append((nx, ny))
 
 n = int(input())
@@ -21,16 +21,15 @@ graph = [list(map(int, input().split())) for _ in range(n)]
 M = max(map(max, graph))
 m = min(map(min, graph))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 a = 0
-for d in range(m-1, M):
+for h in range(m-1, M):
     cnt = 0
     visited = [[False] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-                if graph[i][j] > d and not visited[i][j]:
+                if graph[i][j] > h and not visited[i][j]:
                     dfs(i, j)
                     cnt += 1
     a = max(a, cnt)
