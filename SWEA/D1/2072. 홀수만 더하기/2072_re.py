4a8d45ddb8ae7bdf4616f3a5c00a2491e0fe83de
iblug <1115jkb@gmail.com>
2023-02-20 21:58:00 +0900

    Update

--- SWEA/D1/2072. 홀수만 더하기/2072_re.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-
-for t in range(1, T + 1):
-    # data = 
-    result = [num for num in list(map(int, input().split())) if num % 2]
-    
-    print(f'#{t} {sum(result)}')
-
-# 백준허브 연동 안됨
