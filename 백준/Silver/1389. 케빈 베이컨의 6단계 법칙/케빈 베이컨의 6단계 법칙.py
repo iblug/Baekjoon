b4ea2d90325d48229c4cedad300fe8fa819518a0
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-21 21:27:28 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 60 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(x, d):
+    global c
+    q = deque()
+    q.append((x, d))
+    v[x] = 1
+    while q:
+        x, d = q.popleft()
+        c += d
+        d += 1
+        for e in g[x]:
+            if not v[e]:
+                q.append((e, d))
+                v[e] = 1
+
+n, m = map(int, input().split())
+g = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    g[a].append(b)
+    g[b].append(a)
+
+t = 1e9
+for i in range(1, n+1):
+    v = [0] * (n+1)
+    c = 0
+    bfs(i,0)
+    if c < t:
+       r = i
+       t = c 
+print(r)
