f9406c73950b6085404c68b5121222944397c55a
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-23 00:22:16 +0900

    [Silver II] Title: 유기농 배추, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,33 +1,35 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10000)
 
 def dfs(x, y):
-    graph[x][y] += 1
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0 <= nx < n and 0 <= ny < m:
-            if graph[nx][ny] == 1:
-                dfs(nx, ny)
+    s = [(x, y)]
+    g[x][y] = 0
+    while s:
+        x, y = s.pop()
+        for dx, dy in di:
+            nx = x + dx
+            ny = y + dy
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                continue
+            if g[nx][ny]:
+                s.append((nx, ny))
+                g[nx][ny] = 0
 
-# 상 하 좌 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-t = int(input())
-for _ in range(t):
-    n, m, k = map(int, input().split())
-
-    graph = [[0]*m for _ in range(n)]
-    for _ in range(k):
-        a, b = map(int, input().split())
-        graph[a][b] = 1
-
-    cnt = 0
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1:
+def fnd():
+    c = 0
+    for i in range(N):
+        for j in range(M):
+            if g[i][j]:
                 dfs(i, j)
-                cnt += 1
-    print(cnt)
+                c += 1
+    print(c)
+    
+T = int(input())
+di = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+for _ in range(T):
+    M, N, K = map(int, input().split())
+    g = [[0]*M for _ in range(N)]
+    for _ in range(K):
+        a, b = map(int, input().split())
+        g[b][a] = 1
+    fnd()
