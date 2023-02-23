ec7f9961e6fcce8e7e5a6ca3744932f123934cda
NimbuS <114655005+iblug@users.noreply.github.com>
2023-02-23 16:49:57 +0900

    [Silver III] Title: 세 번 이내에 사과를 먹자, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.py
+++ 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.py
@@ -1,32 +1,23 @@
-import sys, copy
+# 데이터 추가 test
+import sys
 input = sys.stdin.readline
 
-def dfs(x, y, t, a, v):
-    v = [[False]*5 for _ in range(5)]
-    v[x][y] = True
-    s = [(x, y, t, a, v)]
-    while s:
-        x, y, t, a, v = s.pop()
-        if g[x][y] == 1:
-            a += 1
-            if a == 2:
-                return 1
-        if t == 3:
-            continue
-        for dx, dy in d:
-            nx = x + dx
-            ny = y + dy
-            if nx < 0 or nx >= 5 or ny < 0 or ny >= 5:
-                continue
-            if not v[nx][ny] and g[nx][ny] != -1:
-                v[nx][ny] = True
-                v1 = copy.deepcopy(v)
-                s.append((nx, ny, t+1, a,v1))
+g = [list(map(int,input().split())) for _ in range(5)]
+r, c = map(int,input().split())
+d = [(1,0),(-1,0),(0,1),(0,-1)]
+
+def dfs(x,y,t,apple):
+    if apple == 2: print(1); sys.exit()
+    if not t: return
+
+    g[x][y] = -1
+    for dx,dy in d:
+        nx,ny = x+dx, y+dy
+        if 0 <= nx < 5 and 0 <= ny < 5 and g[nx][ny] != -1:
+            if g[nx][ny]:
+                dfs(nx,ny,t-1,apple+1)
+            else:
+                dfs(nx,ny,t-1,apple)
     return 0
 
-g = [list(map(int, input().split())) for _ in range(5)]
-r, c = map(int, input().split())
-
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-print(dfs(r, c, 0, 0, 0))
+print(dfs(r,c,3,0))
