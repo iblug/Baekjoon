dfa9c757cf7d2a4ec2fb653364d0a469cd2d54c6
NimbuS <114655005+iblug@users.noreply.github.com>
2023-03-01 17:39:44 +0900

    [Silver I] Title: 트럭, Time: 92 ms, Memory: 34272 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+n, w, L = map(int, input().split())
+t = list(map(int, input().split()))
+q = deque([0]*w)
+
+i = 0
+time = 0
+while i < n:
+    q.popleft()
+    now = t[i]
+    if sum(q) + now > L:
+        q.append(0)
+    else:
+        q.append(now)
+        i += 1
+    time += 1
+while q:
+    q.popleft()
+    time += 1
+print(time)
