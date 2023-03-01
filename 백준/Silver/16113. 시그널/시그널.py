fa49c579337fedf3eabf73c177f15451ed0e59a3
NimbuS <114655005+iblug@users.noreply.github.com>
2023-03-02 03:39:31 +0900

    [Silver II] Title: 시그널, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16113. 시그널/시그널.py
+++ 백준/Silver/16113. 시그널/시그널.py
@@ -1,41 +1,24 @@
-n = int(input())
-s = input()
-p = n//5
-
-a = ''
-i = 0
-while i < p:
-    if s[i] == '.':
-        i += 1
-        continue
-    if s[i+3*p] == '#':
-        if s[i+p] == '.':
-            t = '2'
-        elif i == p-1:
-            a += '1'
-            break
-        elif s[i+1] == '.':
-            t = '1'
-        elif s[i+1+2*p] == '.':
-            t = '0'
-        elif s[i+2+p] == '.':
-            t = '6'
-        else:
-            t = '8'
-    else:
-        if s[i+1] == '.':
-            t = '4'
-        elif s[i+2*p] == '.':
-            t = '7'
-        elif s[i+2+p] == '.':
-            t = '5'
-        elif s[i+p] == '.':
-            t = '3'
-        else:
-            t = '9'
-    a += t
-    if t == '1':
-        i += 2
-    else:
-        i += 4
+n=int(input())
+s=input()
+p=n//5
+a=''
+i=0
+while i<p:
+ if s[i]=='.':i+=1;continue
+ if s[i+3*p]=='#':
+  if s[i+p]=='.':t='2'
+  elif i==p-1:a+='1';break
+  elif s[i+1]=='.':t='1'
+  elif s[i+1+2*p]=='.':t='0'
+  elif s[i+2+p]=='.':t='6'
+  else:t='8'
+ else:
+  if s[i+1]=='.':t='4'
+  elif s[i+2*p]=='.':t='7'
+  elif s[i+2+p]=='.':t='5'
+  elif s[i+p]=='.':t='3'
+  else:t='9'
+ a+=t
+ if t=='1':i+=2
+ else:i+=4
 print(a)
