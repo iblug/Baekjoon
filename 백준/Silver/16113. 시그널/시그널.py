d0cd0b3803f6615232aeee6620b87665e77ce74d
NimbuS <114655005+iblug@users.noreply.github.com>
2023-03-02 03:16:55 +0900

    [Silver II] Title: 시그널, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16113. 시그널/시그널.py
+++ 백준/Silver/16113. 시그널/시그널.py
@@ -9,21 +9,19 @@ while i < p:
         i += 1
         continue
     if s[i+3*p] == '#':
-        if s[i+p] == '#':
-            if i == p - 1:
-                a += '1'
-                break
-            if s[i+1] == '.':
-                t = '1'
-            else:
-                if s[i+2+p] == '.':
-                    t = '6'
-                elif s[i+1+2*p] == '#':
-                    t = '8'
-                else:
-                    t = '0'
-        else:
+        if s[i+p] == '.':
             t = '2'
+        elif i == p-1:
+            a += '1'
+            break
+        elif s[i+1] == '.':
+            t = '1'
+        elif s[i+1+2*p] == '.':
+            t = '0'
+        elif s[i+2+p] == '.':
+            t = '6'
+        else:
+            t = '8'
     else:
         if s[i+1] == '.':
             t = '4'
