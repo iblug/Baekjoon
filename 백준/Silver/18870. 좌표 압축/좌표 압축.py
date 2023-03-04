bd57c8a37f398a702187000d830e8213e011de5c
NimbuS <114655005+iblug@users.noreply.github.com>
2023-03-04 15:14:56 +0900

    [Silver II] Title: 좌표 압축, Time: 1436 ms, Memory: 202348 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,12 +1,7 @@
-import sys
-input = sys.stdin.readline
-print = sys.stdout.write
-
 n = int(input())
 d = list(map(int, input().split()))
 
 s = sorted(set(d))
 r = {x:i for i, x in enumerate(s)}
 
-for i in d:
-    print(str(r[i])+' ')
+print(' '.join([str(r[i])for i in d]))
