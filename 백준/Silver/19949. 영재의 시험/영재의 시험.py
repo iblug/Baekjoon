098bcda20644943f307a5c1a31aa261fb2f7656b
NimbuS <114655005+iblug@users.noreply.github.com>
2023-03-23 02:58:24 +0900

    [Silver III] Title: 영재의 시험, Time: 2280 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/19949. 영재의 시험/영재의 시험.py
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.py
@@ -1,20 +1,18 @@
 def f(c, p):
-    if (l:=len(c)) == 10:
-        if p >= 5:
-            r[0] += 1
-        return
-
-    for j in range(1, 6):
-        if l >= 2 and c[-2] == c[-1] == j:
-            continue
-        c.append(j)
-        if j == a[l]:
-            f(c, p+1)
-        else:
-            f(c, p)
-        c.pop()
-
-a = list(map(int, input().split()))
-r = [0]
+ if (l:=len(c)) == 10:
+  if p >= 5:
+   r[0] += 1
+  return
+ for j in range(1, 6):
+  if l >= 2 and c[-2] == c[-1] == j:
+   continue
+  c.append(j)
+  if j == a[l]:
+   f(c, p+1)
+  else:
+   f(c, p)
+  c.pop()
+a=list(map(int, input().split()))
+r=[0]
 f([],0)
-print(r[0])
+print(*r)
