a4f7f1cc5ac376af31e22618b765413ce37aea5b
NimbuS <114655005+iblug@users.noreply.github.com>
2023-03-22 21:43:44 +0900

    [Silver III] Title: 근손실, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.py
+++ 백준/Silver/18429. 근손실/근손실.py
@@ -1,16 +1,19 @@
-from itertools import permutations
+def per(u, l, w):
+    global r
+    if l == n:
+        r += 1
+        return
+    w -= k
+    for i in range(n):
+        if not u[i]:
+            if w+a[i] >= 0:
+                u[i] = True
+                per(u, l+1, w+a[i])
+                u[i] = False
 
 n, k = map(int, input().split())
-a = list(map(lambda x:int(x)-k, input().split()))
-a = permutations(a,n)
-c = 0
-for e in a:
-    s = 0
-    for i in e:
-        s += i
-        if s < 0:
-            break
-    else:
-        c += 1
-
-print(c)
+a = list(map(int, input().split()))
+u = [False] * n
+r = 0
+per(u,0,0)
+print(r)
