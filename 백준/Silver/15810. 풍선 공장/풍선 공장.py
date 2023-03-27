cbb37d8a576b9d6814722b4698fb5f512768bf60
NimbuS <114655005+iblug@users.noreply.github.com>
2023-03-27 18:19:16 +0900

    [Silver II] Title: 풍선 공장, Time: 2800 ms, Memory: 150704 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.py
@@ -0,0 +1,19 @@
+def check(mid):
+    c = 0
+    for i in a:
+        c += mid//i
+    return c >= m
+
+def f(start, end):
+    if start + 1 >= end:
+        return end
+    mid = start + end >> 1
+    if check(mid):
+        return f(start, mid)
+    else:
+        return f(mid, end)
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+print(f(0, int(1e13)))
