25fc6ca5facb4ea2a851443a34df8af89788c415
NimbuS <114655005+iblug@users.noreply.github.com>
2023-04-02 16:53:50 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+s = ('a', 'e', 'i', 'o', 'u')
+while True:
+    a = input().rstrip()
+    if a == 'end':
+        break
+    f = 1
+    if not any(i in a for i in s):
+        f = 0
+    for j in range(len(a)-2):
+        if all(a[j+n] in s for n in range(3)):
+            f = 0
+        if all(a[j+m] not in s for m in range(3)):
+            f = 0
+
+    for k in range(len(a)-1):
+        if a[k] not in ('e','o'):
+            if a[k] == a[k+1]:
+                f = 0
+    if f:
+        print(f'<{a}> is acceptable.')
+    else:
+        print(f'<{a}> is not acceptable.')
