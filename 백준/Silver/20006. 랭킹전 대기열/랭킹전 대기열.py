0be460508eb110c08c069ba3949493893b32c805
NimbuS <114655005+iblug@users.noreply.github.com>
2023-04-02 16:22:45 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+p, m = map(int, input().split())
+t = []
+for _ in range(p):
+    a = input().rstrip().split()
+    f = 0
+    a[0] = int(a[0])
+    for i in t:
+        if len(i) == m:
+            continue
+        if i[0][0]-10 <= a[0] <= i[0][0]+10:
+            i.append(a)
+            f = 1
+            break
+    if not f:
+        t.append([a])
+for i in t:
+    if len(i) == m:
+        print('Started!')
+    else:
+        print('Waiting!')
+    k = sorted(i, key=lambda x: x[1])
+    for j in k:
+        print(*j)
