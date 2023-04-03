e6615baf227caa18a95e093dbb3861d262524c64
NimbuS <114655005+iblug@users.noreply.github.com>
2023-04-04 00:06:10 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,7 @@
+d = {
+    'NLCS': 'North London Collegiate School',
+    'BHA': 'Branksome Hall Asia',
+    'KIS': 'Korea International School',
+    'SJA': 'St. Johnsbury Academy'
+}
+print(d[input()])
