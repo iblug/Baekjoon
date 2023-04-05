6766ba5c39ef5417afc559e989bb0d1d1a2aaac4
NimbuS <114655005+iblug@users.noreply.github.com>
2023-04-05 23:33:49 +0900

    [Silver I] Title: 회전 초밥, Time: 2944 ms, Memory: 32748 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, d, k, c = map(int, input().split())
+a = [int(input()) for _ in range(n)]
+a += a
+
+r = 0
+for i in range(n):
+    eat = set(a[i:i+k])
+    if c not in eat:
+        eat.add(c)
+    if len(eat) > r:
+        r = len(eat)
+print(r)
