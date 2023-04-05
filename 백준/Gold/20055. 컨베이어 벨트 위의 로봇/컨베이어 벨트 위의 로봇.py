d66439bae24ffde7b27797ff9e8c3286dcd527b1
NimbuS <114655005+iblug@users.noreply.github.com>
2023-04-06 02:22:48 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 4152 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+n, k = map(int, input().split())
+A = list(map(int, input().split()))
+
+robot_order = deque([])
+robot_belt = [0]*n
+
+cnt = 0
+c = 0
+while c < k:
+    cnt += 1
+    # 1
+    A = A[-1:]+A[:-1]
+    l = len(robot_order)
+
+    if robot_order:
+        for i in range(l):
+            a = robot_order.pop()
+            if a < n-2:
+                if not robot_belt[a+2] and A[a+2] > 0:
+                    robot_order.appendleft(a+2)
+                    robot_belt[a], robot_belt[a+2] = 0, 1
+                    A[a+2] -= 1
+                    if A[a+2] == 0:
+                        c += 1
+                else:
+                    robot_order.appendleft(a+1)
+                    robot_belt[a], robot_belt[a+1] = 0, 1
+            elif a == n-2:
+                robot_belt[a] = 0
+            else:
+                robot_belt[a] = 0
+
+    if A[0] > 0:
+        robot_order.appendleft(0)
+        robot_belt[0] = 1
+        A[0] -= 1
+        if A[0] == 0:
+            c += 1 
+
+print(cnt)
