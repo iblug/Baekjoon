558d9577db129021248377bde18168c1c4303a02
NimbuS <114655005+iblug@users.noreply.github.com>
2023-04-06 22:17:40 +0900

    [Bronze III] Title: TGN, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    r, e, c = map(int, input().split())
+    if r > e-c:
+        print('do not advertise')
+    elif r < e-c:
+        print('advertise')
+    else:
+        print('does not matter')
