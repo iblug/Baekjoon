ef73e81ecf788fd286c8437ad3015cb206fdf748
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-04-19 23:33:52 +0900

    [Gold V] Title: 톱니바퀴, Time: 76 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def right(a, b):
+    if a == 4:
+        return
+    if g[a-1][2] != g[a][6]:
+        right(a+1, b*-1)
+        g[a].rotate(b*-1)
+    else:
+        return
+    return
+
+def left(a, b):
+    if a == -1:
+        return
+    if g[a+1][6] != g[a][2]:
+        left(a-1, b*-1)
+        g[a].rotate(b*-1)
+    else:
+        return
+    return
+
+g = [deque(list(input().rstrip())) for _ in range(4)]
+k = int(input())
+for _ in range(k):
+    a, b = map(int, input().split())
+    
+    right(a, b)
+    left(a-2, b)
+    
+    g[a-1].rotate(b)
+
+c = 1
+r = 0
+for i in range(4):
+    r += int(g[i][0]) * c
+    c *= 2
+print(r)
