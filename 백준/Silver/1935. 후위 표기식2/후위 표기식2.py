3f7d2567df8933ca321e5a28ae6ab5594cc1e5a1
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-04-23 16:39:40 +0900

    [Silver III] Title: 후위 표기식2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -1,13 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-o = ('+', '-', '*', '/')
 n = int(input())
 s = input().rstrip()
 l = [int(input()) for _ in range(n)]
 stack = []
 for j in s:
-    if j in o:
+    if 'A' <= j <= 'Z':
+        stack.append(l[ord(j)-ord('A')])
+    else:
         a = stack.pop()
         if j == '+':
             stack.append(stack.pop()+a)
@@ -17,6 +18,4 @@ for j in s:
             stack.append(stack.pop()*a)
         elif j == '/':
             stack.append(stack.pop()/a)
-    else:
-        stack.append(l[ord(j)-ord('A')])
 print(f'{stack[0]:0.2f}')
