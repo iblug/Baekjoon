80de1a91bc38efb8e22007dd202a43d20cf8c11a
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-04-24 23:32:59 +0900

    [Silver II] Title: 에디터, Time: 340 ms, Memory: 41676 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+s = list(input().rstrip())
+m = int(input())
+r = []
+for _ in range(m):
+    a, *b = input().split()
+    if a == 'L':
+        if s:
+            r.append(s.pop())
+    elif a == 'D':
+        if r:
+            s.append(r.pop())
+    elif a == 'B':
+        if s:
+            s.pop()
+    else:
+        s.append(b[0])
+print(''.join(s+list(reversed(r))))
