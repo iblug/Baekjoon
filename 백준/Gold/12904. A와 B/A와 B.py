92e34db54c3c8cb3a3efb149e2a16020affeca18
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-05-10 23:00:42 +0900

    [Gold V] Title: A와 B, Time: 68 ms, Memory: 34128 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def f(t):
+    q = deque()
+    q.append(t)
+    v.add(t)
+    while q:
+        t = q.popleft()
+        if t == s:
+            return True
+        if len(t) == L:
+            continue
+        if t[-1] == 'B':
+            B = t[:-1][::-1]
+            if B not in v:
+                q.append(B)
+                v.add(B)
+        if t[-1] == 'A':
+            A = t[:-1]
+            if A not in v:
+                q.append(A)
+                v.add(A)
+    return False
+    
+    
+s = input()
+t = input()
+
+L = len(s)
+v = set()
+if f(t):
+    print(1)
+else:
+    print(0)
