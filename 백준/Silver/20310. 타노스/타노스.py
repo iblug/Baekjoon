ef3d494038039ff75e3a464fc35665d7069db648
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-05-23 01:53:32 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -3,17 +3,14 @@ n = len(s)
 c1 = sum(s)
 c0 = (n-c1)//2
 c1 //= 2
-r = [0]*(n//2)
-re = set()
-for i in range(n):
-    if c1:
-        if s[i] == 1:
-            re.add(i)
-            c1 -= 1
-for i in reversed(range(n)):
-    if c0:
-        if s[i] == 0:
-            re.add(i)
-            c0 -= 1
-a = [s[i] for i in range(n) if i not in re]
+r = set()
+for i, v in enumerate(s):
+    if c1 and s[i]:
+        r.add(i)
+        c1 -= 1
+for i, v in reversed(list(enumerate(s))):
+    if c0 and not s[i]:
+        r.add(i)
+        c0 -= 1
+a = (s[i] for i in range(n) if i not in r)
 print(''.join(map(str, a)))
