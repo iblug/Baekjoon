e5b27400444cf8ba8c884327693f15129484a239
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-05-31 19:19:26 +0900

    [Bronze II] Title: 대회 자리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5176. 대회 자리/대회 자리.py
+++ 백준/Bronze/5176. 대회 자리/대회 자리.py
@@ -4,12 +4,6 @@ input = sys.stdin.readline
 K = int(input())
 for _ in range(K):
     P, M = map(int, input().split())
-    s = set()
-    cnt = 0
-    for _ in range(P):
-        a = int(input())
-        if a in s:
-            cnt += 1
-        else:
-            s.add(a)
-    print(cnt)
+    s = {input().rstrip() for _ in range(P)}
+
+    print(P-len(s))
