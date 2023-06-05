1571ca2ab8788594e9c3f58c0ec2c8fbfd117469
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-06-05 17:41:33 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,13 +1,11 @@
 n, b = input().split()
 b = int(b)
 t = 0
-i = len(n) - 1
 d = 0
-while i >= 0:
-    if n[i].isdigit():
-        t += int(n[i])*b**d
+for i in reversed(n):
+    if i.isdigit():
+        t += int(i)*b**d
     else:
-        t += (ord(n[i])-ord('A')+10)*b**d
+        t += (ord(i)-55)*b**d
     d += 1
-    i -= 1
 print(t)
