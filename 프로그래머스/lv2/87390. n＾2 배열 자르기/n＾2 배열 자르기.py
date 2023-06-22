198828a73e75d5e3557e3986219117bcec996852
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-06-22 15:48:49 +0900

    [level 2] Title: n^2 배열 자르기, Time: 843.09 ms, Memory: 340 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -0,0 +1,15 @@
+def solution(n, left, right):
+    answer = []
+    a = left//n
+    aa = left%n
+    b = right//n
+    bb = right%n
+    if a == b:
+        c = [i+1 if i > a else a+1 for i in range(n)]
+        answer = c[aa:bb+1]
+    else:
+        answer += [i+1 if i > a else a+1 for i in range(n)][aa:]
+        for k in range(a+1, b):
+            answer += [i+1 if i > k else k+1 for i in range(n)]
+        answer += [i+1 if i > b else b+1 for i in range(n)][:bb+1]
+    return answer
