8b249bf55e3384a5ade82cb07cacb0627694d61c
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-07-18 21:19:12 +0900

    [Gold V] Title: AC, Time: 144 ms, Memory: 45016 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    p = input().rstrip()
+    n = int(input().rstrip())
+    c = p.count('D')
+    if c > n:
+        input()
+        print('error')
+        continue
+    
+    x = input().rstrip().replace('[','').replace(']','').split(',')
+    f = 0
+    s = 0
+    e = n
+    for i in p:
+        if i == 'R':
+            f ^= 1
+        else:
+            if f:
+                e -= 1
+            else:
+                s += 1
+    if f:
+        print(f'[{",".join(reversed(x[s:e]))}]')
+    else:
+        print(f'[{",".join(x[s:e])}]')
