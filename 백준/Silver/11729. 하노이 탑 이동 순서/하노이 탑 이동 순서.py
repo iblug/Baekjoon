efb0d721340b7121448d0541aac493edcde61384
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-07-19 19:31:47 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 400 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,15 +1,15 @@
 import sys
-sys.setrecursionlimit(int(1e6))
+print = sys.stdout.write
 
 def h(n, start, mid, end):
     if n == 1:
-        print(start, end)
+        print(f'{start} {end}\n')
         return
     
     h(n-1, start, end, mid)
-    print(start, end)
+    print(f'{start} {end}\n')
     h(n-1, mid, start, end)
 
 n = int(input())
-print(2**n-1)
+print(f'{2**n-1}\n')
 h(n, 1, 2, 3)
