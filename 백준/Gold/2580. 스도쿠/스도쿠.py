4f4e8e2242162bfd6cfbda4cdb7e621635db25fb
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-07-26 13:16:53 +0900

    [Gold IV] Title: 스도쿠, Time: 3196 ms, Memory: 200516 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -7,32 +7,27 @@ def s(bnk):
             print(' '.join(map(str,b[i])))
         exit()
     x, y = blank[bnk]
-    for i in range(1, 10):
-        if check_r(x, i) and check_c(y, i) and check_b(x, y, i):
-            b[x][y] = i
-            s(bnk+1)
-            b[x][y] = 0
+    c = check(x, y)
+    for i in c:
+        b[x][y] = i
+        s(bnk+1)
+        b[x][y] = 0
 
-def check_r(x, t):
+def check(x, y):
+    s = {i for i in range(1, 10)}
+
     for i in range(9):
-        if t == b[x][i]:
-            return False
-    return True
-
-def check_c(y, t):
-    for i in range(9):
-        if t == b[i][y]:
-            return False
-    return True
-
-def check_b(x, y, t):
+        if b[x][i] in s:
+            s.discard(b[x][i])
+        if b[i][y] in s:
+            s.discard(b[i][y])
     xx = x//3 * 3
     yy = y//3 * 3
     for i in range(3):
         for j in range(3):
-            if b[xx+i][yy+j] == t:
-                return False
-    return True
+            if b[xx+i][yy+j] in s:
+                s.discard(b[xx+i][yy+j])
+    return s
 
 b = [list(map(int, input().split())) for _ in range(9)]
 blank = []
