ae25039b7a8b83d3c62a774563d7f62ff81b760a
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-07-31 22:06:39 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -4,11 +4,7 @@ input = sys.stdin.readline
 t = int(input())
 lst = [list(map(int, input().split())) for _ in range(t)]
 for i in range(1, t):
-    for j in range(3):
-        if j == 0:
-            lst[i][0] = lst[i][0] + min(lst[i-1][1], lst[i-1][2])
-        if j == 1:
-            lst[i][1] = lst[i][1] + min(lst[i-1][0], lst[i-1][2])
-        if j == 2:
-            lst[i][2] = lst[i][2] + min(lst[i-1][0], lst[i-1][1])
+    lst[i][0] = lst[i][0] + min(lst[i-1][1], lst[i-1][2])
+    lst[i][1] = lst[i][1] + min(lst[i-1][0], lst[i-1][2])
+    lst[i][2] = lst[i][2] + min(lst[i-1][0], lst[i-1][1])
 print(min(lst[-1]))
