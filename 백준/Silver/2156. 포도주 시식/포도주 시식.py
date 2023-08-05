e39dc356020dbac635b8bfc07d9f520fce9a92e8
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-05 16:07:36 +0900

    [Silver I] Title: 포도주 시식, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,15 +1,12 @@
 import sys
 input = sys.stdin.readline
-
 n = int(input())
 d = [int(input()) for _ in range(n)]
-dp = [0]*n
-
-dp[0] = d[0]
+dp = [d[0]]
 if n > 1:
-    dp[1] = d[0]+d[1]
+    dp.append(d[0]+d[1])
 if n > 2:
-    dp[2] = max(d[2]+d[1], d[2]+d[0], dp[1])
-for i in range(3, n):
-    dp[i] = max(dp[i-1], dp[i-3]+d[i-1]+d[i], dp[i-2]+d[i])
+    dp.append(max(d[2]+d[1], d[2]+d[0], dp[1]))
+    for i in range(3, n):
+        dp.append(max(dp[i-2]+d[i], dp[i-3]+d[i-1]+d[i], dp[i-1]))
 print(dp[n-1])
