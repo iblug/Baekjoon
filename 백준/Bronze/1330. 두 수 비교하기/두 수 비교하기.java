12ecf8e8fd312ac8ef0ebb1a93178150c03269c0
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-05 17:11:54 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 132 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] data = br.readLine().split(" ");
+		int a = Integer.parseInt(data[0]);
+		int b = Integer.parseInt(data[1]);
+		if (a < b) {
+			System.out.print("<");
+		} else if (a > b){
+			System.out.print(">");
+		} else {
+			System.out.print("==");
+		}
+	}
+
+}
