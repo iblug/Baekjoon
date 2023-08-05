aec867eceb4d75587b6a5d14a3e3f46721abe39b
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-05 20:10:50 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -3,9 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static void main(String[] args) throws IOException {
 		new Main().solution();
+		br.close();
 	}
 	private void solution() throws IOException {
 		int x = Integer.parseInt(br.readLine());
@@ -14,18 +15,14 @@ public class Main {
 		System.out.print(getQ(x, y));
 	}
 	private int getQ(int x, int y) {
-		if (x * y > 0) {
-			if (x > 0) {
-				return 1;
-			} else {
-				return 3;
-			}
+		if (x > 0 && y > 0) {
+			return 1;
+		} else if (x < 0 && y > 0) {
+			return 2;
+		} else if (x < 0 && y < 0) {
+			return 3;
 		} else {
-			if (x < 0) {
-				return 2;
-			} else {
-				return 4;
-			}
+			return 4;
 		}
 	}
 }
