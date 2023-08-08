8e98b2c1877b9402084b69e36ac50677f279718c
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-08 23:20:15 +0900

    [Silver IV] Title: 스택 2, Time: 936 ms, Memory: 101328 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,23 +1,24 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
-s = []
+s = deque()
 for _ in range(N):
-    d = list(map(int, input().split()))
-    if d[0] == 1:
+    d = input().split()
+    if d[0] == '1':
         s.append(d[1])
-    elif d[0] == 2 and s:
+    elif d[0] == '2' and s:
         print(s.pop())
-    elif d[0] == 2:
+    elif d[0] == '2':
         print(-1)
-    elif d[0] == 3:
+    elif d[0] == '3':
         print(len(s))
-    elif d[0] == 4 and s:
+    elif d[0] == '4' and s:
         print(0)
-    elif d[0] == 4:
+    elif d[0] == '4':
         print(1)
-    elif d[0] == 5 and s:
+    elif d[0] == '5' and s:
         print(s[-1])
     elif d[0]:
         print(-1)
