1f730e76d5109e83f426a0a9156c9fc9d474510f
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-09 22:16:52 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		new Main().solution();
+	}
+
+	private void solution() throws NumberFormatException, IOException {
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append(a+b).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
