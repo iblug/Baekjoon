42d870115fce1c6280ed5071bc808b4ec2be6b35
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-10 14:57:49 +0900

    [Bronze V] Title: 오늘 날짜, Time: 200 ms, Memory: 17248 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -2,7 +2,6 @@ import java.time.LocalDate;
 
 public class Main {
 	public static void main(String[] args) {
-		LocalDate d1 = LocalDate.now();
-		System.out.println(d1);
+		System.out.println(LocalDate.now());
 	}
 }
