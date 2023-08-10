10334d189f5993f8b14c0700d3bfec5cd6fd3b73
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-10 17:46:23 +0900

    [Bronze V] Title: 사파리월드, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		new Main().solution();
+	}
+	private void solution() throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long a = Integer.parseInt(st.nextToken());
+		long b = Integer.parseInt(st.nextToken());
+		long c = a-b;
+		if (c<0) {
+			System.out.println(-c);
+		} else {
+			System.out.println(c);
+		}
+		
+	}
+}
