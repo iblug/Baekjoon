03604a8459c3d8fd5c3323c8f3fdb6abc89806e6
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-11 22:21:00 +0900

    [Bronze V] Title: 합, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		new Main().solution();
+	}
+	public void solution() throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(n*(n+1)/2);
+	}
+}
