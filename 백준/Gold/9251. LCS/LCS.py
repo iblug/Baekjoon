41b7fb1debe844cd6612affaf86ee517dfe5ad2f
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-17 22:52:40 +0900

    [Gold V] Title: LCS, Time: 568 ms, Memory: 56408 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,7 +1,13 @@
-n = ' '+input()
-m = ' '+input()
+import sys
+input = sys.stdin.readline
+
+n = ' '+input().rstrip()
+m = ' '+input().rstrip()
 d = [[0 for _ in n] for _ in m]
 for y in range(1, len(m)):
     for x in range(1, len(n)):
-        d[y][x] = max(d[y-1][x], d[y][x-1], d[y-1][x-1]+(n[x] == m[y]))
+        if n[x] == m[y]:
+            d[y][x] = d[y-1][x-1]+1
+        else:
+            d[y][x] = max(d[y-1][x], d[y][x-1])
 print(d[-1][-1])
