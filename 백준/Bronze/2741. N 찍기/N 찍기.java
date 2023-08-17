c4aceb850d65cb9b693bd6503eaab293ce840a09
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-17 09:03:14 +0900

    [Bronze V] Title: N 찍기, Time: 232 ms, Memory: 17996 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		new Main().solution();
+		br.close();
+	}
+	private void solution() throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= n; i++) {
+			sb.append(i).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
