11f5b22394e06afe9c8308b703202260043356e9
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-17 09:10:21 +0900

    [Bronze V] Title: 팩토리얼, Time: 128 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int r = 1;
+		for (int i = 1; i <= n; i++) {
+			r *= i;
+		}
+		System.out.print(r);
+	}
+}
