df9afaa033ba4464120525786a5e8207e633cc72
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-20 18:52:56 +0900

    [Bronze II] Title: 공 바꾸기, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,37 +1,38 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+// https://www.acmicpc.net/source/63709474
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static void main(String[] args) throws IOException {
-		new Main().solution();
-	}
-	private void solution() throws IOException {
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		int[] arr = new int[N+1];
-		for (int i = 1; i <= N; i++) {
-			arr[i] = i; 
-		}
-		
-		int a, b, t;
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			t = arr[a];
-			arr[a] = arr[b];
-			arr[b] = t;
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= N; i++) {
-			sb.append(arr[i]).append(" ");
-		}
-		System.out.println(sb);
-	}
-	
+    public static void main(String[] args) throws IOException {
+        final int size = readInt();
+        final int count = readInt();
+
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < count; i++) {
+            final int a = readInt() - 1;
+            final int b = readInt() - 1;
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.print(sb);
+    }
+
+    private static int readInt() throws IOException {
+        int val = 0;
+        int total = 0;
+
+        while ((val = System.in.read()) != '\n' && val != ' ') {
+            total = total * 10 + (val - '0');
+        }
+        return total;
+    }
 }
