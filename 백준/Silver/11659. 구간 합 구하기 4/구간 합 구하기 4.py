bca3970b100a2cbd46f770cd9777b3c722dc8c01
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-20 13:31:59 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 216 ms, Memory: 50536 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -4,10 +4,8 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 d = list(map(int, input().split()))
 s = [0]
-su = 0
 for i in range(N):
-    su += d[i]
-    s.append(su)
+    s.append(s[i]+d[i])
 r = []
 for _ in range(M):
     i, j = map(int, input().split())
