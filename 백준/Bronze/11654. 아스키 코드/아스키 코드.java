c6f10100f8b341ab3e7a16750a3c4287d28814a2
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-08-29 22:01:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -2,10 +2,6 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		new Main().solution();
-	}
-
-	private void solution() throws IOException {
 		System.out.println(System.in.read());
 	}
 }
