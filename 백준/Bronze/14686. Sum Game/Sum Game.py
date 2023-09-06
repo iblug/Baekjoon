ad9194fe6fe3027087e15ac15d49f8225d34851d
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-09-06 19:24:06 +0900

    [Bronze II] Title: Sum Game, Time: 100 ms, Memory: 39144 KB -BaekjoonHub

--- 백준/Bronze/14686. Sum Game/Sum Game.py
+++ 백준/Bronze/14686. Sum Game/Sum Game.py
@@ -1,12 +1,12 @@
 N = int(input())
-w = [0] + list(map(int, input().split()))
-e = [0] + list(map(int, input().split()))
+w = list(map(int, input().split()))
+e = list(map(int, input().split()))
 
 cnt = 0
 ww = ee = 0
-for i in range(N+1):
+for i in range(N):
     ww += w[i]
     ee += e[i]
     if ww == ee:
-        cnt = i 
+        cnt = i +1 
 print(cnt)
