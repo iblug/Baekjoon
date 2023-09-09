0f2aa1292cd0fd74fa6a97cf6ae66170d3719920
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-09-09 18:00:09 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		new Main().solution();
+	}
+
+	private void solution() throws IOException {
+		int a = readInt();
+		int b = readInt();
+		
+		System.out.println(a*a - b*b);
+	}
+
+	private int readInt() throws IOException {
+		int v, t = 0;
+		while ((v = System.in.read()) > 47) {
+			t = t * 10 + (v - 48);
+		}
+		
+		return t;
+	}
+}
