fe4fb1b8f661745cb673b7d0009a98f3e35c8843
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-09-11 18:47:16 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1076 ms, Memory: 106008 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+d = [list(map(int, input().split())) for _ in range(n)]
+a = [[0]*(n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        a[i][j] = a[i-1][j] + a[i][j-1] + d[i-1][j-1] - a[i-1][j-1]
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(a[x2][y2] - a[x1-1][y2] - a[x2][y1-1] + a[x1-1][y1-1])
