eee5fcde8e8ccebf23b72f1bb67844e72661da8c
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-09-12 18:22:51 +0900

    [Silver I] Title: 회의실 배정, Time: 244 ms, Memory: 52036 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -3,18 +3,12 @@ input = sys.stdin.readline
 
 n = int(input())
 c = [tuple(map(int, input().split())) for _ in range(n)]
-c = sorted(c, key=lambda x: (x[0], x[1]))
-stk = []
-for e in c:
-    while stk:
-        if e[1] < stk[-1][1]:
-            stk.pop()
-        else:
-            break
-    if stk:
-        if e[0] >= stk[-1][1]:
-            stk.append(e)
-    else:
-            stk.append(e)
+c.sort(key=lambda x:(x[1], x[0]))
+cnt = 1
+e = c[0][1]
 
-print(len(stk))  
+for i in c[1:]:
+    if i[0] >= e:
+        cnt += 1
+        e = i[1]
+print(cnt)
