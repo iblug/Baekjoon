30c322cc9f690fdbc175a050a63b64a68847fba1
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-11-06 23:24:55 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.py
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.py
@@ -5,13 +5,7 @@ n = int(input())
 print('Gnomes:')
 for i in range(n):
     a, b, c = map(int, input().split())
-    if a < b:
-        if b < c:
-            print('Ordered')
-        else:
-            print('Unordered')
+    if a < b < c or a > b > c:
+        print('Ordered')
     else:
-        if b > c:
-            print('Ordered')
-        else:
-            print('Unordered')
+        print('Unordered')
