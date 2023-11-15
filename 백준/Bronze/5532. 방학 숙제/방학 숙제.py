a3d7b32f0dd83680420d5d07a4c20c2ed8bf924d
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-11-15 12:04:56 +0900

    [Bronze IV] Title: 방학 숙제, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.py
@@ -0,0 +1,9 @@
+import sys, math
+input = sys.stdin.readline
+
+l = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(l - max(math.ceil(a/c),math.ceil(b/d)))
