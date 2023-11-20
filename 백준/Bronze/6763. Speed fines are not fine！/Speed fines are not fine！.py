8add2ee8a665cf07448db1cf2c21bf58907d39cb
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-11-20 22:02:58 +0900

    [Bronze IV] Title: Speed fines are not fine!, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6763. Speed fines are not fine！/Speed fines are not fine！.py
@@ -0,0 +1,10 @@
+a = int(input()) - int(input())
+
+if a >= 0:
+    print('Congratulations, you are within the speed limit!')    
+elif a >= -20:
+    print('You are speeding and your fine is $100.')
+elif a >= -30:
+    print('You are speeding and your fine is $270.')
+else:
+    print('You are speeding and your fine is $500.')
