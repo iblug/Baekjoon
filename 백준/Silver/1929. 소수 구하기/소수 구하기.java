173ec413e9495df69881c6e93514a74a2be3cc35
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-11-24 14:47:55 +0900

    [Silver III] Title: 소수 구하기, Time: 244 ms, Memory: 20380 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		new Main().solution();
+	}
+
+	private void solution() throws IOException {
+		int a = readInt();
+		int b = readInt();
+		boolean[] f = new boolean[b+1];
+		
+		Arrays.fill(f, true);
+		f[0] = false;
+		f[1] = false;
+		
+		for (int i = 1; i < Math.sqrt(b) + 1; i++) {
+			if (f[i]) {
+				for (int j = i*2; j <= b; j += i) {
+					f[j] = false;
+				}
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		if (a <= 2 && b >= 2) {
+			sb.append(2).append("\n");
+			a = 3;
+		}
+		if (a % 2 == 0) {
+			a++;
+		}
+		for (int i = a; i <= b; i += 2) {
+			if (f[i]) {
+				sb.append(i).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+	private int readInt() throws IOException {
+		int v, t = 0;
+		while ((v = System.in.read()) > 47) {
+			 t = t * 10 + (v - 48);
+		}
+		return t;
+	}
+}
