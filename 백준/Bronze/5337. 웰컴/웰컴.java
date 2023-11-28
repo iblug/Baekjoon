d662ec14c4d04828febf163562f8f1e6e6d09de9
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-11-28 19:43:19 +0900

    [Bronze V] Title: 웰컴, Time: 120 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		new Main().solution();
+	}
+
+	private void solution() {
+		System.out.println(".  .   .\n"
+				+ "|  | _ | _. _ ._ _  _\n"
+				+ "|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
