7fbf60a2e11ea0a3aff3470a716eb5631c0f877c
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-12-09 14:04:51 +0900

    [Bronze IV] Title: 특별한 날, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,14 @@
+m = int(input())
+d = int(input())
+
+if m == 1:
+    print('Before')
+elif m == 2:
+    if d < 18:
+        print('Before')
+    elif d > 18:
+        print('After')
+    else:
+        print('Special')
+else:
+    print('After')
