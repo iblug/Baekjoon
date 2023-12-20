a34bdadc5a685f4f15ba9ca84ecec3a157a1b30e
Nimbu$ <114655005+iblug@users.noreply.github.com>
2023-12-20 20:01:42 +0900

    [Bronze V] Title: 연세대학교, Time: 116 ms, Memory: 13848 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		int b = System.in.read();
+		if (b == '0') {
+			System.out.println("YONSEI");			
+		} else {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+}
