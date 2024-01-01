6fee6d09ce9548280ea2305f969e7159e7cbc222
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-01-01 17:00:46 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 124 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.java
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.java
@@ -1,24 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    final String[] e = new String[] {"a", "e", "i", "o", "u"};
+    static final List<Character> e = Arrays.asList('a', 'e', 'i', 'o', 'u');
+
     public static void main(String[] args) throws IOException {
         new Main().solution();
     }
 
     private void solution() throws IOException {
-        br.readLine();
+        int n = Integer.parseInt(br.readLine());
         String s = br.readLine();
 
         int r = 0;
-        for (String v : e) {
-            r += (int) s.chars()
-                    .filter(i -> String.valueOf((char) i).equals(v))
-                    .count();
+        for (int i = 0; i < n; i++) {
+            if (e.contains(s.charAt(i))) {
+                r++;
+            }
         }
+
         System.out.println(r);
     }
 }
