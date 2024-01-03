1f84dddad1d991bc1b5967af4b3626a9a4227445
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-01-03 09:23:47 +0900

    [Bronze V] Title: 세금, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/20492. 세금/세금.java
+++ 백준/Bronze/20492. 세금/세금.java
@@ -1,18 +1,22 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws IOException {
         new Main().solution();
     }
 
     private void solution() throws IOException {
-        int n = Integer.parseInt(br.readLine());
+        int n = readInt();
         StringBuilder sb = new StringBuilder();
         sb.append(n/1000 * 780).append(' ').append(n/1000 * (800 + 2 * 78));
         System.out.println(sb);
+    }
+
+    private int readInt() throws IOException {
+        int v, t = 0;
+        while ((v = System.in.read()) > 47) {
+            t = 10 * t + v - 48;
+        }
+        return t;
     }
 }
