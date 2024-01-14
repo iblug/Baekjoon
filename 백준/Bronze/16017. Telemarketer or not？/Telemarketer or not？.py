41a922e98fb887618566e1a422e7f205b06c8618
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-01-14 17:24:57 +0900

    [Bronze IV] Title: Telemarketer or not?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/16017. Telemarketer or not？/Telemarketer or not？.py
+++ 백준/Bronze/16017. Telemarketer or not？/Telemarketer or not？.py
@@ -3,9 +3,7 @@ input = sys.stdin.readline
 
 s = [input().rstrip() for _ in range(4)]
 
-if any([s[0] not in '89', s[3] not in '89']):
-    print('answer')
-elif s[1] != s[2]:
-    print('answer')
-else:
+if all([s[0] in '89', s[3] in '89']) and s[1] == s[2]:
     print('ignore')
+else:
+    print('answer')
