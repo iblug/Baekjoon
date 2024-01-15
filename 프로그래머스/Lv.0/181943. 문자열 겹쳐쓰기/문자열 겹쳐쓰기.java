eb9d403ba940b62caab3449a3e621a68e2487bd9
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-01-15 13:41:48 +0900

    [level Lv.0] Title: 문자열 겹쳐쓰기, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string.substring(0, s)).append(overwrite_string).append(my_string.substring(s + overwrite_string.length()));
+        return sb.toString();
+    }
+}
