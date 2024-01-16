062e218f8e30da0ad9ce1d9cc614771280da3e6b
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-01-16 19:46:34 +0900

    [level Lv.0] Title: 조건 문자열, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        if (n == m) {
+            if (eq.equals("=")) {
+                answer = 1;
+            }
+        } else if (n > m) {
+            if (ineq.equals(">")) {
+                answer = 1;
+            }
+        } else {
+            if (ineq.equals("<")) {
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+}
