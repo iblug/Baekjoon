55b5a02b472ebc519237ac99358d2276039032d9
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-01-17 10:45:30 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 220 ms, Memory: 15356 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,16 +1,10 @@
 import java.io.IOException;
 
 public class Main {
-    static boolean[] prime;
-    static int NUM = 1000001;
-
     public static void main(String[] args) throws IOException {
-        prime = new boolean[NUM];
-        prime[0] = true;
-        prime[1] = true;
+        StringBuilder sb = new StringBuilder();
+        boolean[] prime = getPrime();
         int t = readInt();
-
-        getPrime();
 
         while (t-- > 0) {
             int n = readInt();
@@ -20,18 +14,24 @@ public class Main {
                     c++;
                 }
             }
-            System.out.println(c);
+            sb.append(c).append('\n');
         }
+
+        System.out.println(sb);
     }
 
-    static void getPrime() {
-        for (int i = 2; i < Math.sqrt(NUM) + 1; i++) {
+    static boolean[] getPrime() {
+        boolean[] prime = new boolean[1000001];
+        prime[0] = true;
+        prime[1] = true;
+        for (int i = 2; i < Math.sqrt(1000001) + 1; i++) {
             if (!prime[i]) {
-                for (int j = i * 2; j < NUM; j += i) {
+                for (int j = i * 2; j < 1000001; j += i) {
                     prime[j] = true;
                 }
             }
         }
+        return prime;
     }
 
     static int readInt() throws IOException {
