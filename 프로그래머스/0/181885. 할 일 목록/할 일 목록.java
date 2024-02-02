05e642bf0cc5ff19a3f16727a68ce2698ae44626
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-02-02 18:47:58 +0900

    [level 0] Title: 할 일 목록, Time: 0.39 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -3,18 +3,13 @@ import java.util.ArrayList;
 
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        List<String> list = new ArrayList<>();
+        List<String> answer = new ArrayList<>();
         for (int i = 0; i < todo_list.length; i++) {
             if (!finished[i]) {
-                list.add(todo_list[i]);
+                answer.add(todo_list[i]);
             }
         }
-
-        String[] answer = new String[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-
-        return list.toArray(new String[0]);
+        
+        return answer.toArray(String[]::new);
     }
 }
