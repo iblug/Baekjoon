41265cf440efecddab2f420deb16e3df7ad0b8ab
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-02-16 10:34:22 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 348 ms, Memory: 22072 KB -BaekjoonHub

--- 백준/Gold V/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold V/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -2,11 +2,13 @@ import java.io.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        System.out.println((int) Math.pow(2, n) - 1);
+        bw.write(String.valueOf((int) Math.pow(2, n) - 1));
+        bw.write('\n');
 
         hanoi(n, '1', '2', '3');
 
@@ -17,16 +19,16 @@ public class Main {
     static void hanoi(int n, char s, char m, char e) throws IOException {
         if (n == 1) {
             bw.write(s);
-            bw.write(" ");
+            bw.write(' ');
             bw.write(e);
-            bw.write("\n");
+            bw.write('\n');
             return;
         }
         hanoi(n - 1, s, e, m);
         bw.write(s);
-        bw.write(" ");
+        bw.write(' ');
         bw.write(e);
-        bw.write("\n");
+        bw.write('\n');
         hanoi(n - 1, m, s, e);
     }
 }
