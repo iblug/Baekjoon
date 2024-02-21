ccd7108d09e2f8827d4540b0e661857e6998e467
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-02-21 20:17:39 +0900

    [Bronze IV] Title: Rulltrappa, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/20867. Rulltrappa/Rulltrappa.py
+++ 백준/Bronze/20867. Rulltrappa/Rulltrappa.py
@@ -2,7 +2,4 @@ m, s, g = map(int, input().split())
 a, b = map(float, input().split())
 l, r = map(int, input().split())
 
-if m/g + l/a < m/s + r/b:
-    print('friskus')
-else:
-    print('latmask')
+print('flraitsmkaussk'[m/g + l/a > m/s + r/b ::2])
