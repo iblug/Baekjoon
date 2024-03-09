a9a634a739b0c8543154bc200902d824b8a6ddf1
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-03-09 19:37:51 +0900

    [Silver III] Title: 01타일, Time: 136 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(fibo(n));
+    }
+
+    static int fibo(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        int a = 1;
+        int b = 2;
+        int result = 0;
+
+        for (int i = 2; i < n; i++) {
+            result = (a + b) % 15746;
+            a = b;
+            b = result;
+        }
+
+        return result;
+    }
+}
