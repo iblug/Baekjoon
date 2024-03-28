0a5472a9f8d7942d98bbe7bc0ed7f30c34f0818d
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-03-28 17:31:25 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 364 ms, Memory: 27168 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,23 +1,17 @@
 import java.io.IOException;
 
 public class Main {
-    static int[][] arr, dp;
+    static int[][] dp;
     static int n, m;
 
     public static void main(String[] args) throws IOException {
         n = readInt();
         m = readInt();
-        arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = readInt();
-            }
-        }
 
         dp = new int[n+1][n+1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i-1][j-1] - dp[i-1][j-1];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] + readInt() - dp[i-1][j-1];
             }
         }
 
