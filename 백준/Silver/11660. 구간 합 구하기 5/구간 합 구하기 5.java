b9b25d7b1ba7d6e8417e5a3791837e8be65a7f7d
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-03-28 17:29:56 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 416 ms, Memory: 32136 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+
+public class Main {
+    static int[][] arr, dp;
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        n = readInt();
+        m = readInt();
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = readInt();
+            }
+        }
+
+        dp = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i-1][j-1] - dp[i-1][j-1];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int x1, y1, x2, y2;
+        while (m-- > 0) {
+            x1 = readInt();
+            y1 = readInt();
+            x2 = readInt();
+            y2 = readInt();
+
+            sb.append(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    static int readInt() throws IOException {
+        int v, t = 0;
+        while ((v = System.in.read()) > 47) {
+            t = t * 10 + (v - 48);
+        }
+        if (v == 13) {
+            System.in.read();
+        }
+        return t;
+    }
+}
