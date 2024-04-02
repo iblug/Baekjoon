ec1fc882fd502b80f9b882c767710bd4ddd2617b
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-04-02 17:06:44 +0900

    [Bronze V] Title: Correct, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println((readInt() - 9) * 60 + readInt());
+    }
+
+    static int readInt() throws IOException {
+        int v, t = 0;
+        while ((v = System.in.read()) > 47) {
+            t = t * 10 + (v - 48);
+        }
+        return t;
+    }
+}
