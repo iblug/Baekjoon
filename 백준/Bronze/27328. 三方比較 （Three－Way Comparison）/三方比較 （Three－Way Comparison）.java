9cf127809cbbab5469245e095bad65775560adde
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-04-13 21:14:56 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 116 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int a = readInt();
+        int b = readInt();
+
+        if (a > b) {
+            System.out.println(1);
+        } else if (a == b) {
+            System.out.println(0);
+        } else {
+            System.out.println(-1);
+        }
+
+    }
+
+    private static int readInt() throws IOException {
+        int value = 0;
+        byte v;
+        while ((v = read()) <= ' ') ;
+        do {
+            value = value * 10 + v - '0';
+        } while ((v = read()) >= '0' && v <= '9');
+        return value;
+    }
+
+    static final int SIZE = 1 << 13;
+    static byte[] buffer = new byte[SIZE];
+    static int index, size;
+    private static byte read() throws IOException {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+
+}
