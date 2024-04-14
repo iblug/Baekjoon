33dfe1edbf22bc7014378fdf9b6dcae64488ade0
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-04-14 23:17:55 +0900

    [Bronze V] Title: Rectangles, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.java
@@ -0,0 +1,42 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int r = readInt();
+        int c = readInt();
+
+        for (int i = 0; i < r; i++) {
+            bw.write("*".repeat(c));
+            bw.write('\n');
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static int readInt() throws IOException {
+        int value = 0;
+        byte v;
+        while ((v = read()) <= ' ') ;
+        do {
+            value = value * 10 + v - '0';
+        } while ((v = read()) >= '0' && v <= '9');
+        return value;
+    }
+
+    static final int SIZE = 1 << 13;
+    static byte[] buffer = new byte[SIZE];
+    static int index, size;
+    private static byte read() throws IOException {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
