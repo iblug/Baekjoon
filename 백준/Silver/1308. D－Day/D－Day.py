07bc20facae4be2da0ac82434429452f3a506781
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-04-19 11:19:18 +0900

    [Silver V] Title: D-Day, Time: 64 ms, Memory: 38048 KB -BaekjoonHub

--- 백준/Silver/1308. D－Day/D－Day.py
+++ 백준/Silver/1308. D－Day/D－Day.py
@@ -5,11 +5,7 @@ start = dt.date(y1, m1, d1)
 y2, m2, d2 = map(int, input().split())
 end = dt.date(y2, m2, d2)
 
-if y2 - y1 > 1000:
-    print('gg')
-elif y2 - y1 == 1000 and m1 < m2:
-    print('gg')
-elif y2 - y1 == 1000 and m1 == m2 and d1 <= d2:
+if end >= dt.date(y1+1000, m1, d1):
     print('gg')
 else:
     print(f'D-{(end - start).days}')
