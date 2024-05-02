aae79819ec1ffa1ec3bbdd6a1946cecf7665f36a
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-05-02 14:30:52 +0900

    [Bronze IV] Title: Fold the Paper Nicely, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/26340. Fold the Paper Nicely/Fold the Paper Nicely.py
+++ 백준/Bronze/26340. Fold the Paper Nicely/Fold the Paper Nicely.py
@@ -1,17 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-for _ in range(n):
+for _ in range(int(input())):
     a, b, c = map(int, input().split())
     print(f'Data set: {a} {b} {c}')
     for _ in range(c):
         if a > b:
-            a = a / 2
+            a, b = a//2, b
         else:
-            b = b / 2
-    a, b = int(a), int(b)
-    if a < b:
-        a, b = b, a
-    print(a, b)
+            a, b = a, b//2
+    if a > b:
+            a, b = b, a
+    print(b, a)
     print()
