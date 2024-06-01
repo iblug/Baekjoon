00a36e615c2bea9edbc5d2d4f927dbf243ca8a8e
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-06-01 12:30:16 +0900

    [Bronze IV] Title: Cornhole, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27855. Cornhole/Cornhole.py
@@ -0,0 +1,10 @@
+h1, b1 = map(int, input().split())
+h2, b2 = map(int, input().split())
+s1 = h1*3 + b1
+s2 = h2*3 + b2
+if s1 > s2:
+    print(1, s1-s2)
+elif s2 > s1:
+    print(2, s2-s1)
+else:
+    print("NO SCORE")
