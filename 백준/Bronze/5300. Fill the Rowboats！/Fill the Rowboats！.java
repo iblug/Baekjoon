aa9b5acf7db7d947f84a62a948b79d5e745c4dc3
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-06-02 23:16:31 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 112 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(i).append(" ");
+            if (i % 6 == 0) {
+                sb.append("Go! ");
+            }
+        }
+        if (n % 6 != 0) {
+            sb.append("Go!");
+        }
+
+        System.out.println(sb);
+    }
+}
