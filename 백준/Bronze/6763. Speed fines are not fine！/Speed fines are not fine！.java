d6564e741897efb99a7a6e48c9c98db9a010af1f
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-06-16 23:46:42 +0900

    [Bronze IV] Title: Speed fines are not fine!, Time: 120 ms, Memory: 13988 KB -BaekjoonHub

--- 백준/Bronze/6763. Speed fines are not fine！/Speed fines are not fine！.java
+++ 백준/Bronze/6763. Speed fines are not fine！/Speed fines are not fine！.java
@@ -3,12 +3,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         int l = readInt();
-        int s = readInt();
-        if (s - l > 30) {
+        int r = readInt() - l;
+        if (r > 30) {
             System.out.println("You are speeding and your fine is $500.");
-        } else if (s - l > 20) {
+        } else if (r > 20) {
             System.out.println("You are speeding and your fine is $270.");
-        } else if (s - l > 0) {
+        } else if (r > 0) {
             System.out.println("You are speeding and your fine is $100.");
         } else {
             System.out.println("Congratulations, you are within the speed limit!");
