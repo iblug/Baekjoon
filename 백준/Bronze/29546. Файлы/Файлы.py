8d9b9140189e6c51b14014d44c77045f7bb1e4b0
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-06-16 23:32:19 +0900

    [Bronze IV] Title: Файлы, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29546. Файлы/Файлы.py
+++ 백준/Bronze/29546. Файлы/Файлы.py
@@ -2,8 +2,8 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-a = [0]+[input().rstrip() for _ in range(n)]
+a = [input().rstrip() for _ in range(n)]
 m = int(input())
 for _ in range(m):
     l, r = map(int, input().split())
-    [print(a[i]) for i in range(l, r+1)]
+    print('\n'.join(a[l-1:r]))
