24adb2a9672a4fe23651cf661c881c8556013a74
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-07-03 16:25:57 +0900

    [Bronze IV] Title: Quadrants, Time: 116 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/9772. Quadrants/Quadrants.java
+++ 백준/Bronze/9772. Quadrants/Quadrants.java
@@ -4,40 +4,32 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        float x, y;
-        StringTokenizer st;
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        float x, y;
+
         while (true) {
             st = new StringTokenizer(br.readLine());
             x = Float.parseFloat(st.nextToken());
             y = Float.parseFloat(st.nextToken());
 
-            bw.write(Quadrant(x, y));
-
-            if (x == 0 && y == 0) {
+            if (x > 0 && y > 0) {
+                bw.write("Q1\n");
+            } else if (x < 0 && y > 0) {
+                bw.write("Q2\n");
+            } else if (x < 0 && y < 0) {
+                bw.write("Q3\n");
+            } else if (x > 0 && y < 0) {
+                bw.write("Q4\n");
+            } else if (x == 0 && y ==0) {
+                bw.write("AXIS\n");
                 break;
+            } else {
+                bw.write("AXIS\n");
             }
-
-            bw.write("\n");
         }
 
         bw.flush();
         bw.close();
-    }
-
-    static String Quadrant(float x, float y) {
-        String result;
-        if (x > 0 && y > 0) {
-            result = "Q1";
-        } else if (x < 0 && y > 0) {
-            result = "Q2";
-        } else if (x < 0 && y < 0) {
-            result = "Q3";
-        } else if (x > 0 && y < 0) {
-            result = "Q4";
-        } else {
-            result = "AXIS";
-        }
-        return result;
     }
 }
