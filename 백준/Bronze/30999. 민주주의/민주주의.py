9b3f261e62f887be61f08bc6632a8ad5ea375067
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-07-07 15:13:30 +0900

    [Bronze IV] Title: 민주주의, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30999. 민주주의/민주주의.py
+++ 백준/Bronze/30999. 민주주의/민주주의.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 s = 0
 for _ in range(n):
-    if input().count('O') > m/2:
+    a = input()
+    if a.count('O') > m/2:
         s += 1
 print(s)
