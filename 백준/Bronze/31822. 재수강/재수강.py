97470f5acf7ab57f2c0d82b261f5c701f8451585
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-08-02 01:33:57 +0900

    [Bronze IV] Title: 재수강, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/31822. 재수강/재수강.py
+++ 백준/Bronze/31822. 재수강/재수강.py
@@ -1,11 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-g = input()
+g = input()[:5]
 n = int(input())
 r = 0
 for i in range(n):
     s = input()
-    if s.startswith(g[:5]):
+    if s.startswith(g):
         r += 1
 print(r)
