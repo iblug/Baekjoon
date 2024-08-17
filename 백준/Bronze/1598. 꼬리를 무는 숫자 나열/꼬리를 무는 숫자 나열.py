e6112fa4d799c4482ecbf51154b0ddc85cf00883
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-08-17 17:52:54 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.py
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.py
@@ -1,8 +1,4 @@
 n, m = map(int, input().split())
-
-x1 = n//4 if n%4 else (n//4) - 1
-y1 = n % 4 if n%4 else 4
-x2 = m//4 if m%4 else (m//4) - 1
-y2 = m % 4 if m%4 else 4
-
-print(abs(x2 - x1) + abs(y2 - y1))
+n -= 1
+m -= 1
+print(abs(n//4 - m//4) + abs(n%4 - m%4))
