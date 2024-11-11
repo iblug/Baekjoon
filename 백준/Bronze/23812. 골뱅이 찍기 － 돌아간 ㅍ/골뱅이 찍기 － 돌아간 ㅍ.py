5d9951b7cfe1367ad7e0d1ff169e4dbb95368d21
Nimbu$ <114655005+iblug@users.noreply.github.com>
2024-11-12 05:02:11 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for _ in range(n):
+    print('@'*n + ' '*(3*n) + '@'*n)
+for _ in range(n):
+    print('@'*(5*n))
+for _ in range(n):
+    print('@'*n + ' '*(3*n) + '@'*n)
+for _ in range(n):
+    print('@'*(5*n))
+for _ in range(n):
+    print('@'*n + ' ' * (3*n) + '@'*n)
